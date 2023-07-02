8c577e14a3243170e0d045db4e485a6bd615a03d
Fruxz <cedricspitzer@outlook.de>
2023-07-02 15:08:31 +0200

    chore: base project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,113 @@
 plugins {
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "1.9.0-RC"
+    id("org.jetbrains.dokka") version "1.8.20"
+    `maven-publish`
 }
 
-group = "de.fruxz.brigadikt"
-version = "1.0-SNAPSHOT"
+val host = "github.com/TheFruxz/BrigadiKt"
+
+version = "2023.3-dev"
+group = "dev.fruxz"
 
 repositories {
+
     mavenCentral()
+
+    maven("https://jitpack.io") {
+        name = "JitPack"
+    }
+
+    maven("https://libraries.minecraft.net") {
+        name = "Minecraft Libraries"
+    }
+
 }
 
 dependencies {
+
+    // Kotlin
     testImplementation(kotlin("test"))
+    implementation(kotlin("reflect"))
+
+    // KotlinX
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+
+    // MoltenKt
+    implementation("com.github.TheFruxz:Ascend:2023.2")
+    implementation("com.github.TheFruxz:Stacked:2023.2b")
+
+    // Brigadier
+    api("com.mojang:brigadier:1.0.18")
+
 }
 
-tasks.test {
-    useJUnitPlatform()
+val dokkaHtmlJar by tasks.register<Jar>("dokkaHtmlJar") {
+    dependsOn(tasks.dokkaHtml)
+    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    archiveClassifier.set("html-docs")
+}
+
+val dokkaJavadocJar by tasks.register<Jar>("dokkaJavadocJar") {
+    dependsOn(tasks.dokkaJavadoc)
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+val sourceJar by tasks.register<Jar>("sourceJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+
+    repositories {
+
+        mavenLocal()
+
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.$host")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+
+    }
+
+    publications.create("BrigadiKt", MavenPublication::class) {
+        artifactId = "brigadikt"
+        version = version.lowercase()
+
+        artifact(dokkaJavadocJar)
+        artifact(dokkaHtmlJar)
+        artifact(sourceJar) {
+            classifier = "sources"
+        }
+
+        from(components["kotlin"])
+
+    }
+
+}
+
+tasks {
+
+    test {
+        useJUnitPlatform()
+    }
+
+    dokkaHtml.configure {
+        outputDirectory.set(buildDir.resolve("../docs/"))
+    }
+
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
