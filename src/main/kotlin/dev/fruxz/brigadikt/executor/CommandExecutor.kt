f711d44dfb5b0f461a670e3ebebd299b9d9ba3e1
Fruxz <cedricspitzer@outlook.de>
2025-03-26 05:12:37 +0100

    feature: add perform functions to command- and requirement executor
    avoiding big code explosions

--- src/main/kotlin/dev/fruxz/brigadikt/executor/CommandExecutor.kt
+++ src/main/kotlin/dev/fruxz/brigadikt/executor/CommandExecutor.kt
@@ -3,5 +3,9 @@ package dev.fruxz.brigadikt.executor
 import dev.fruxz.brigadikt.CommandContext
 
 fun interface CommandExecutor {
+
     fun CommandContext.execution()
+
+    fun perform(context: CommandContext) = with(context) { execution() }
+
 }
--- src/main/kotlin/dev/fruxz/brigadikt/executor/RequirementExecutor.kt
+++ src/main/kotlin/dev/fruxz/brigadikt/executor/RequirementExecutor.kt
@@ -3,5 +3,9 @@ package dev.fruxz.brigadikt.executor
 import dev.fruxz.brigadikt.RequirementContext
 
 fun interface RequirementExecutor {
+
     fun RequirementContext.requirement(): Boolean
+
+    fun perform(context: RequirementContext): Boolean = with(context) { requirement() }
+
 }
