44551398d2ff940f4ee7fc2cb1c621f31d3c2d19
Fruxz <cedricspitzer@outlook.de>
2025-03-26 05:11:37 +0100

    feature: argument instruction results now share results as parameter

--- src/main/kotlin/dev/fruxz/brigadikt/structure/ArgumentInstructionResult.kt
+++ src/main/kotlin/dev/fruxz/brigadikt/structure/ArgumentInstructionResult.kt
@@ -2,14 +2,19 @@ package dev.fruxz.brigadikt.structure
 
 import dev.fruxz.brigadikt.PaperArgBuilder
 
-sealed interface ArgumentInstructionResult<T>
+sealed interface ArgumentInstructionResult<T> {
+    val instruction: ArgumentInstruction<*>
+    val results: List<PaperArgBuilder>
+}
 
 data class MultipleArgumentInstructionResult(
-    val instruction: ArgumentInstruction<*>,
-    val results: List<PaperArgBuilder>
+    override val instruction: ArgumentInstruction<*>,
+    override val results: List<PaperArgBuilder>
 ) : ArgumentInstructionResult<List<PaperArgBuilder>>
 
 data class SingleArgumentInstructionResult(
-    val instruction: ArgumentInstruction<*>,
+    override val instruction: ArgumentInstruction<*>,
     val result: PaperArgBuilder
-) : ArgumentInstructionResult<PaperArgBuilder>
+) : ArgumentInstructionResult<PaperArgBuilder> {
+    override val results: List<PaperArgBuilder> = listOf(result)
+}
